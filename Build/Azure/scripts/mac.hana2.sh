6612b4d69ec42686d05354c4164e5c40c7684e91
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-23 09:58:49 +0100

    try to fix quotes

--- Build/Azure/scripts/mac.hana2.sh
+++ Build/Azure/scripts/mac.hana2.sh
@@ -33,9 +33,10 @@ docker logs hana2
 # clear memory limits
 ~/linq2db_ci/providers/saphana/macos/HDBSQL/hdbsql -d HXE -n localhost:39013 -u SYSTEM -p Passw0rd ALTER USER SYSTEM CLEAR PARAMETER STATEMENT MEMORY LIMIT
 # create linked server for FQN names testing
-~/linq2db_ci/providers/saphana/macos/HDBSQL/hdbsql -d HXE -n localhost:39013 -u SYSTEM -p Passw0rd CREATE REMOTE SOURCE "LINKED_DB" ADAPTER "hanaodbc" CONFIGURATION 'DRIVER=libodbcHDB.so;ServerNode=127.0.0.1:39015;'
-~/linq2db_ci/providers/saphana/macos/HDBSQL/hdbsql -d HXE -n localhost:39013 -u SYSTEM -p Passw0rd CREATE CREDENTIAL FOR USER SYSTEM COMPONENT 'SAPHANAFEDERATION' PURPOSE 'LINKED_DB' TYPE 'PASSWORD' USING 'user=SYSTEM;password=Passw0rd';
-
+sql = 'CREATE REMOTE SOURCE "LINKED_DB" ADAPTER "hanaodbc" CONFIGURATION \'DRIVER=libodbcHDB.so;ServerNode=127.0.0.1:39013;\''
+~/linq2db_ci/providers/saphana/macos/HDBSQL/hdbsql -d HXE -n localhost:39013 -u SYSTEM -p Passw0rd '$sql'
+sql = 'CREATE CREDENTIAL FOR USER SYSTEM COMPONENT \'SAPHANAFEDERATION\' PURPOSE \'LINKED_DB\' TYPE \'PASSWORD\' USING \'user=SYSTEM;password=Passw0rd\''
+~/linq2db_ci/providers/saphana/macos/HDBSQL/hdbsql -d HXE -n localhost:39013 -u SYSTEM -p Passw0rd '$sql'
 
 cat <<-EOJSON > UserDataProviders.json
 {
